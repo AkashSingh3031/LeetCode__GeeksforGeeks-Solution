9ade3e6a03399350a9d9860a4decd07baec52609
Akash Singh <akashsingh27101998@gmail.com>
2022-05-20 16:46:28 +0530

    Time: 79 ms (69.09%), Space: 15.1 MB (63.87%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         d = {}
         for i, n in enumerate(nums):
             m = target - n
