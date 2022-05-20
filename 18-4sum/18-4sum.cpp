706af0178aeb7535bce7758aa1b438568563e552
Akash Singh <akashsingh27101998@gmail.com>
2022-05-20 20:18:41 +0530

    Time: 14 ms (96.88%), Space: 13.5 MB (33.42%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        def index(node):
+            if not node:
+                return 0
+            i = index(node.next) + 1
+            if i > n:
+                node.next.val = node.val
+            return i
+        index(head)
+        return head.next
