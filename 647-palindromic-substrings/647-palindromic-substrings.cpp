cdd1d1945e2986411fae8caa214733ba9664a23c
Akash Singh <akashsingh27101998@gmail.com>
2022-05-22 12:10:44 +0530

    Time: 7 ms (72.70%), Space: 6.2 MB (96.68%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int res = 0, n = s.length();
+        for(int i = 0; i < n; i++){
+            for(int j = 0; i-j >= 0 && i+j < n && s[i-j] == s[i+j]; j++)res++; //substring s[i-j, ..., i+j]
+            for(int j = 0; i-1-j >= 0 && i+j < n && s[i-1-j] == s[i+j]; j++)res++; //substring s[i-1-j, ..., i+j]
+        }
+        return res;
+    }
+};
