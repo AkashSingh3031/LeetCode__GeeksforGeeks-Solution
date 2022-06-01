217449588eca4edb5bbe08ebb471bb09b8ebe7dc
Akash Singh <akashsingh27101998@gmail.com>
2022-06-01 13:22:08 +0530

    Time: 67 ms (66.77%), Space: 14 MB (98.07%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,17 +1,15 @@
-class Solution
-{
-  public:
-    vector<int> twoSum(vector<int>& nums, int target)
-    {
-        unordered_map<int, int> hash;
-        for(int i=0; i<nums.size(); i++)
-        {
-            int n = nums[i], m = target-n;
-            if(hash.find(m) != hash.end())
-                return {hash[m], i};
-            else
-                hash[n] = i;
-        }
-        return {};
-    }
-};
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen = {}
+        for i, firstNumber in enumerate(nums):
+            secondNumber = target - firstNumber # firstNumber + secondNumber = target
+            
+            if secondNumber in seen:
+                return [seen[secondNumber], i]
+            else:
+                seen[firstNumber] = i
