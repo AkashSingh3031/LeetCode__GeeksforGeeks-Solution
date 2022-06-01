8eec1c8ed0adaed621a3f0bd295806d95cd974a9
Akash Singh <akashsingh27101998@gmail.com>
2022-06-01 12:33:23 +0530

    Time: 15 ms (73.19%), Space: 10.9 MB (46.02%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -6,9 +6,11 @@ class Solution
         unordered_map<int, int> hash;
         for(int i=0; i<nums.size(); i++)
         {
-            if(hash.find(target-nums[i]) != hash.end())
-                return {hash[target-nums[i]], i};
-            hash[nums[i]] = i;
+            int n = nums[i], m = target-n;
+            if(hash.find(m) != hash.end())
+                return {hash[m], i};
+            else
+                hash[n] = i;
         }
         return {};
     }
