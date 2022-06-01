d00d7347585be38203e28f0fd02ad92e8e637943
Akash Singh <akashsingh27101998@gmail.com>
2022-06-01 23:14:26 +0530

    Time: 929 ms (12.35%), Space: 49.9 MB (16.10%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,18 +4,17 @@ public:
         unordered_set<int> num;
         int ans = 0;
         
-        for(int i=0; i<nums.size(); i++) {
-            num.insert(nums[i]);
+        for(int i : nums) {
+            num.insert(i);
         }
         
-        for(int i=0; i<nums.size(); i++) {
-            int x = nums[i];
-            if(num.find(x-1) == num.end()) {
-                int curr = 1;
-                while(num.find(x+curr) != num.end()) {
-                    curr++;
+        for(int i : nums) {
+            if(!num.count(i-1)) {
+                int count = 1;
+                while(num.count(i+count)) {
+                    count++;
                 }
-                ans = max(ans, curr);
+                ans = max(ans, count);
             }
         }
         return ans;
