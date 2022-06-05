90a8ff1b3cd969c0f8af0103482f9567e2ad20c5
Akash Singh <akashsingh27101998@gmail.com>
2022-06-05 09:53:57 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (70.93%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -5,12 +5,16 @@ public:
         int low = 0, mid = 0, high = n-1;
         
         while(mid <= high) {
-            if(nums[mid] == 0)
-                swap(nums[low++], nums[mid++]);
-            else if(nums[mid] == 1)
-                mid++;
-            else 
-                swap(nums[mid], nums[high--]);
+            switch(nums[mid]) {
+                case 0:
+                    swap(nums[low++], nums[mid++]);
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    swap(nums[mid], nums[high--]);
+            }
         }
     }
 };
