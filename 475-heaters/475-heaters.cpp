8cca1c8ef8702e4d735966ee4d92a4eb1508f617
Akash Singh <akashsingh27101998@gmail.com>
2022-06-09 17:52:31 +0530

    Time: 77 ms (65.82%), Space: 25.8 MB (19.61%) - LeetHub

---
+++ 475-heaters/475-heaters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findRadius(vector<int>& A, vector<int>& H) {
+        sort(A.begin(), A.end());
+        sort(H.begin(), H.end());
+        vector<int> res(A.size(), INT_MAX); 
+        
+        // For each house, calculate distance to nearest RHS heater
+        for (int i = 0, h = 0; i < A.size() && h < H.size(); ) {
+            if (A[i] <= H[h]) { res[i] = H[h] - A[i]; i++; }
+            else { h++; }
+        }
+        
+        // For each house, calculate distance to nearest LHS heater
+        for (int i = A.size()-1, h = H.size()-1; i >= 0 && h >= 0; ) {
+            if (A[i] >= H[h]) { res[i] = min(res[i], A[i] - H[h]); i--; }
+            else { h--; }
+        }
+       
+        return *max_element(res.begin(), res.end());
+    }
+};
