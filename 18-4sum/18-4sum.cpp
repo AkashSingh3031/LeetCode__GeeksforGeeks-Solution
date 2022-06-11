809ce13748e5d5295a87e3df4220ce333779ddce
Akash Singh <akashsingh27101998@gmail.com>
2022-06-12 00:02:26 +0530

    Time: 78 ms (66.75%), Space: 13.1 MB (75.67%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,21 +1,46 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: ListNode
-        :type n: int
-        :rtype: ListNode
-        """
-        def index(node):
-            if not node:
-                return 0
-            i = index(node.next) + 1
-            if i > n:
-                node.next.val = node.val
-            return i
-        index(head)
-        return head.next
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<vector<int>> res;
+        
+        if(nums.empty())
+            return res;
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<n; i++) {
+            int target3 = target-nums[i];
+            for(int j=i+1; j<n; j++) {
+                int target2 = target3 - nums[j];
+                int front = j+1;
+                int back = n-1;
+                
+                while(front < back) {
+                    int twoSum = nums[front] + nums[back];
+                    if(twoSum < target2)
+                        front++;
+                    else if(twoSum > target2)
+                        back--;
+                    else {
+                        vector<int> r(4, 0);
+                        r[0] = nums[i];
+                        r[1] = nums[j];
+                        r[2] = nums[front];
+                        r[3] = nums[back];
+                        res.push_back(r);
+                        
+                        while(front < back and nums[front] == r[2])
+                            front++;
+                        while(front < back and nums[back] == r[3])
+                            back--;
+                    }
+                }
+                while(j+1 < n and nums[j+1] == nums[j])
+                    j++;
+            }
+            while(i+1 < n and nums[i+1] == nums[i])
+                i++;
+        }
+        return res;
+    }
+};
