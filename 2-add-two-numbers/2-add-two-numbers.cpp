b0dda7a25b0ffc35f655ddbf9ba925277958be76
Akash Singh <akashsingh27101998@gmail.com>
2022-06-14 20:35:25 +0530

    Time: 77 ms (10.77%), Space: 72.4 MB (11.70%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -18,12 +18,12 @@ public:
 		else if (l2 == NULL) 
             return l1; 
 
-		int a = l1->val + l2->val;
-		ListNode *p = new ListNode(a % 10);
-		p->next = addTwoNumbers(l1->next,l2->next);
+		int add = l1->val + l2->val;
+		ListNode *res = new ListNode(add % 10);
+		res->next = addTwoNumbers(l1->next, l2->next);
         
-		if (a >= 10) 
-            p->next = addTwoNumbers(p->next, new ListNode(1));
-        return p;
+		if (add >= 10) 
+            res->next = addTwoNumbers(res->next, new ListNode(1));
+        return res;
     }
 };
