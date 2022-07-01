5aede6cf0e659617118cbae2ffa656048881d848
Akash Singh <akashsingh27101998@gmail.com>
2022-07-01 19:03:40 +0530

    Time: 42 ms, Memory: 17.9 MB - LeetHub

---
+++ missing-number/missing-number.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        long n = nums.size();
+        return n * (n+1) / 2 - accumulate(begin(nums), end(nums), 0);
+    }
+};
