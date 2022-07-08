9152a38b7fd85f94727510515d0dbede297832ec
Akash Singh <akashsingh27101998@gmail.com>
2022-07-08 16:12:35 +0530

    Added solution - LeetHub

--- Job Sequencing Problem - GFG/job-sequencing-problem.cpp
+++ Job Sequencing Problem - GFG/job-sequencing-problem.cpp
@@ -27,28 +27,31 @@ class Solution
 {
     public:
     //Function to find the maximum profit and the number of jobs done.
-    static bool cmp(Job &a,Job &b){
+    bool static comparator(Job &a,Job &b){
         return a.profit>b.profit;
     }
     vector<int> JobScheduling(Job arr[], int n) 
-   { 
+    { 
        // your code here
         vector<int>v;
         if(n==0){
             return v;
         }
-        sort(arr,arr+n,cmp);
-        int maxi=0;
+        
+        sort(arr,arr+n,comparator);
+        int max_deadline = 0;
         for(int i=0;i<n;i++){
-            maxi=max(maxi,arr[i].dead);
+            max_deadline = max(max_deadline, arr[i].dead);
         }
+        
         int jobprofit=0;
         int jobcount=0;
-        vector<int>slot(maxi+1,-1);
+        vector<int>filled(max_deadline+1, -1);
+        
         for(int i=0;i<n;i++){
             for(int j=arr[i].dead;j>0;j--){
-                if(slot[j]==-1){
-                    slot[j]=i;
+                if(filled[j]==-1){
+                    filled[j]=i;
                     jobprofit+=arr[i].profit;
                     jobcount++;
                     break;
