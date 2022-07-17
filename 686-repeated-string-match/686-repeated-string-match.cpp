25da2f18549a1682423734322be4b8af8819f8e8
Akash Singh <akashsingh27101998@gmail.com>
2022-07-17 16:00:41 +0530

    Time: 653 ms (15.15%), Space: 6.7 MB (92.52%) - LeetHub

---
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int repeatedStringMatch(string A, string B) {
+        for (auto i = 0, j = 0; i < A.size(); ++i) {
+            for (j = 0; j < B.size() && A[(i + j) % A.size()] == B[j]; ++j);
+            if (j == B.size()) 
+                return (i + j - 1) / A.size() + 1;
+        }
+        return -1;
+    }
+};
