8475e817db228771c78005aafd71fae4fade1ddf
Akash Singh <akashsingh27101998@gmail.com>
2022-07-18 11:35:06 +0530

    Time: 44 ms (28.31%), Space: 14.3 MB (42.20%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> G(n);
-        vector<int> degree(n, 0), bfs;
-        for (auto& e : prerequisites)
-            G[e[1]].push_back(e[0]), degree[e[0]]++;
-        for (int i = 0; i < n; ++i) 
-            if (!degree[i])
-                bfs.push_back(i);
-        for (int i = 0; i < bfs.size(); ++i)
-            for (int j: G[bfs[i]])
-                if (--degree[j] == 0) 
-                    bfs.push_back(j);
-        return bfs.size() == n;
+    bool iscycle(vector<int> adj[],vector<int> &vis,int id){
+        if(vis[id]==1)
+            return true;
+        if(vis[id]==0){
+            vis[id]=1;
+            for(auto edge : adj[id]){
+                if(iscycle(adj,vis,edge))
+                    return true;
+            }
+        }
+        vis[id] = 2;
+        return false;
+    }
+    bool canFinish(int n, vector<vector<int>>& pre) {
+        vector<int> adj[n];
+        for(auto edge : pre)
+            adj[edge[1]].push_back(edge[0]);
+        vector<int> vis(n,0);
+        
+        for(int i=0;i<n;i++){
+            if(iscycle(adj,vis,i))
+                return false;
+        }
+        return true;
     }
 };
