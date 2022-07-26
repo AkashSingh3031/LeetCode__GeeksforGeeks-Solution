88181e1168b0763c92363dd701a3c560d0c5fa23
Akash Singh <akashsingh27101998@gmail.com>
2022-07-27 00:03:02 +0530

    Time: 92 ms (11.21%), Space: 89.3 MB (60.88%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -2,37 +2,31 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-        int n = nums1.size();
-        int m = nums2.size();
-        
-        if(m<n)
-            return findMedianSortedArrays(nums2, nums1);
-        
-        int s = 0;
-        int e = n;
-        int total = n+m;
-        
-        while(s<=e) {
-            int mid1 = (s+e)/2;
-            int mid2 = (total+1)/2 - mid1;
-            
-            int l1 = (mid1 == 0 ? INT_MIN : nums1[mid1-1]);
-            int l2 = (mid2 == 0 ? INT_MIN : nums2[mid2-1]);
-            
-            int r1 = (mid1 == n ? INT_MAX : nums1[mid1]);
-            int r2 = (mid2 == m ? INT_MAX : nums2[mid2]);
-            
-            if(l1<=r2 && l2<=r1) {
-                if(total % 2 == 0)
-                    return (max(l1, l2) + min(r1, r2)) / 2.0;
-                else
-                    return max(l1, l2);
-            }
-            else if(l1 > r2)
-                e = mid1-1;
-            else
-                s = mid1+1;
+        const int n1 = nums1.size();
+        const int n2 = nums2.size();
+        if (n1 > n2)
+          return findMedianSortedArrays(nums2, nums1);
+
+        int l = 0;
+        int r = n1;
+
+        while (l <= r) {
+          const int partition1 = (l + r) / 2;
+          const int partition2 = (n1 + n2 + 1) / 2 - partition1;
+          const int maxLeft1 = partition1 == 0 ? INT_MIN : nums1[partition1 - 1];
+          const int maxLeft2 = partition2 == 0 ? INT_MIN : nums2[partition2 - 1];
+          const int minRight1 = partition1 == n1 ? INT_MAX : nums1[partition1];
+          const int minRight2 = partition2 == n2 ? INT_MAX : nums2[partition2];
+          if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1)
+            return (n1 + n2) % 2 == 0
+                       ? (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) * 0.5
+                       : max(maxLeft1, maxLeft2);
+          else if (maxLeft1 > minRight2)
+            r = partition1 - 1;
+          else
+            l = partition1 + 1;
         }
-        return 0.0;
+
+        throw;
     }
 };
