1518e52189498bb85e9379c0cbd70883659ff48a
Akash Singh <akashsingh27101998@gmail.com>
2022-08-15 00:11:38 +0530

    Time: 14 ms (59.87%), Space: 9.4 MB (89.89%) - LeetHub

---
+++ 685-redundant-connection-ii/685-redundant-connection-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> parent(n+1, 0), candA, candB;
+        // step 1, check whether there is a node with two parents
+        for (auto &edge:edges) {
+            if (parent[edge[1]] == 0)
+                parent[edge[1]] = edge[0]; 
+            else {
+                candA = {parent[edge[1]], edge[1]};
+                candB = edge;
+                edge[1] = 0;
+            }
+        } 
+        // step 2, union find
+        for (int i = 1; i <= n; i++) parent[i] = i;
+        for (auto &edge:edges) {
+            if (edge[1] == 0) continue;
+            int u = edge[0], v = edge[1], pu = root(parent, u);
+            // Now every node only has 1 parent, so root of v is implicitly v
+            if (pu == v) {
+                if (candA.empty()) return edge;
+                return candA;
+            }
+            parent[v] = pu;
+        }
+        return candB;
+    }
+private:
+    int root(vector<int>& parent, int k) {
+        if (parent[k] != k) 
+            parent[k] = root(parent, parent[k]);
+        return parent[k];
+    }
+};
